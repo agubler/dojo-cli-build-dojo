005feabb02c621324d373986818067962ec6a8c8
Anthony Gubler <agubler@sitepen.com>
2016-10-02 20:40:54 +0100

    tweak logging

--- src/run.ts
+++ src/run.ts
@@ -1,5 +1,6 @@
 import { Argv } from 'yargs';
 import { Helper } from 'dojo-cli/interfaces';
+import * as chalk from 'chalk';
 
 export interface DojoBuildArgs extends Argv {
 	type: 'dev' | 'dist';
@@ -14,12 +15,14 @@ export default async function(helper: Helper, args: DojoBuildArgs) {
 	const cleanDirectory = args.type === 'dev' ? '_build' : 'dist';
 
 	if (installedCommands) {
-
+		console.info(chalk.underline(`Cleaning target directory \'${cleanDirectory}\'\n`));
 		await helper.command.run('clean', 'file', <any> { files: cleanDirectory });
 		// TODO check conectivity before removing and installing typings
-		await helper.command.run('clean', 'file', <any> { files: 'typings/' });
+		console.info(chalk.underline('Cleaning \'typings\' directory\n'));
+		await helper.command.run('clean', 'file', <any> { files: 'typings' });
 		await helper.command.run('install', 'typings', <any> { directory: process.cwd() });
 		await helper.command.run('compile', 'ts', args);
+		console.info(chalk.green.bold(`\n${args.type} build completed!`));
 	}
 	else {
 		throw new Error('Required commands not installed.');
